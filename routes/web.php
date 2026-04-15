95f97cf170d1c77ec34a301189239816ef727bb0
agikwiyono <zackyclagent61@gmail.com>
2026-04-16 02:54:09 +0700

    askdbaisduefyasxaxaxaxs

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/storage/reports/{filename}', function ($filename) {
+    // Cari alamat asli file-nya
+    $path = storage_path('app/public/reports/' . $filename);
+    
+    // Jika tidak ada, kembalikan error 404
+    if (!File::exists($path)) {
+        abort(404);
+    }
+    
+    // Jika ada, kirimkan file-nya sebagai gambar
+    $file = File::get($path);
+    $type = File::mimeType($path);
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+    
+    return $response;
 });
